756ac6e3d906c5df465f5f93c46c638a0a51070c
Alexandru <raducanu.alexandru2001@gmail.com>
2023-04-24 14:07:34 +0300

    CEO of Chess

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/pages/Home.js';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home.js";
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home}/>
-      </Switch>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Login" exact component={Login} />
+        </Switch>
       </Router>
     </>
   );
