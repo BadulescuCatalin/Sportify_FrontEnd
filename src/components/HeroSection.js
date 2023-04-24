2904f79245436e9d5c3c3445957bd081354ae507
andrei.badulescu01 <andrei.badulescu01@stud.acs.upb.ro>
2023-04-24 13:38:29 +0300

    FE Dev

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <h1>SPORTING LIKE THERE IS NO TOMORROW</h1>
+      <p>Got Game?</p>
+      </div>
+  );
+}
+
+export default HeroSection;
