600ce4579d164be10d401029a72da004e4cdd3c3
stefannitulescu <snitulescu2001@yahoo.com>
2023-04-26 23:57:46 +0300

    logged+navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./App.css";
+import NavbarLogged from "./components/NavbarLogged";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home.js";
 import Login from "./components/pages/Login.js";
 import Register from "./components/pages/Register.js";
+import HomeLogged from "./components/pages/HomeLogged";
 
 function App() {
   return (
     <>
       <Router>
-        <Navbar />
+        <NavbarLogged />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/HomeLogged" exact component={HomeLogged} />
           <Route path="/Login" exact component={Login} />
           <Route path="/Register" exact component={Register} />
         </Switch>
---
+++ src/components/HomeLoggedSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+import "./HomeLoggedSection.css";
+
+function HomeLoggedSection() {
+  return (
+    <div className="hero-container">
+      <h1>Daten kilu meu</h1>
+      <p>Got Game?</p>
+    </div>
+  );
+}
+
+export default HomeLoggedSection;
