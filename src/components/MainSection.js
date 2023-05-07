dac3de2f794a6f763627b88576e2a857f562ccfa
Alexandru <raducanu.alexandru2001@gmail.com>
2023-05-07 12:50:13 +0300

    partial FEDEV 7 mai

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -7,25 +7,59 @@ import TextComponent from "./TextComponent";
 function MainSection() {
   return (
     <div className="main-container">
-      <TextComponent
-        image="../teren_fotbal.jpg"
-        textButon="Book it!"
-        textMare="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... "
-        textMic="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-      />
-      <img src={require("../teren_fotbal.jpg")} />
-      <TextComponent
-        image="../teren_fotbal.jpg"
-        textButon="Book it!"
-        textMare="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... "
-        textMic="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-      />
-      <TextComponent
-        image="../teren_fotbal.jpg"
-        textButon="Book it!"
-        textMare="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... "
-        textMic="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-      />
+      <div className="section-container section-container1">
+        <TextComponent
+          textButon="Book it!"
+          textMare="Do you want to reserve a  sport court?"
+          textMic="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+        />
+        <img
+          style={{
+            width: "300px",
+            height: "300px",
+            borderRadius: "20px",
+            position: "relative",
+            left: "-8%",
+          }}
+          src={require("../teren_fotbal.jpg")}
+        />
+      </div>
+
+      <div className="section-container section-container2">
+        <img
+          style={{
+            width: "300px",
+            height: "300px",
+            borderRadius: "20px",
+            position: "relative",
+            left: "8%",
+          }}
+          src={require("../team.jpg")}
+        />
+        <TextComponent
+          textButon="Find a team!"
+          textMare="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... "
+          textMic="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+        />
+      </div>
+
+      <div className="section-container section-container2">
+        <TextComponent
+          textButon="Manage courts!"
+          textMare="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... "
+          textMic="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+        />
+        <img
+          style={{
+            width: "300px",
+            height: "300px",
+            borderRadius: "20px",
+            position: "relative",
+            left: "-8%",
+          }}
+          src={require("../manage.jpg")}
+        />
+      </div>
     </div>
   );
 }
