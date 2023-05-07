b2e954bbf40625a667ed3348b9513c3f2ef24d5f
Alexandru <raducanu.alexandru2001@gmail.com>
2023-05-07 16:47:10 +0300

    FEDEV 7 mai ora 17

--- src/components/pages/Book.js
+++ src/components/pages/Book.js
@@ -1,6 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navigation from "../Navigation";
+import Court from "../Court";
 
 function Book() {
-  return <></>;
+  const [dataArray, setDataArray] = useState([]);
+  axios({
+    url: "http://localhost:8080/fields",
+    method: "GET",
+    // TODO Token?????????
+  }).then((res) => {
+    setDataArray(res.data);
+  });
+  return (
+    <div>
+      <div>
+        <Navigation></Navigation>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          className="courts-container"
+          style={{
+            marginTop: "10%",
+            width: "30%",
+            position: "relative",
+          }}
+        >
+          {dataArray.map((item) => (
+            <Court obiect={item} key={item.key} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Book;
