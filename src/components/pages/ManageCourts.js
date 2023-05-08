cc3dccab8a06ac98066dfe5aaa24b30e59121487
Alexandru <raducanu.alexandru2001@gmail.com>
2023-05-08 19:09:53 +0300

    FeDev Delete functionality done

--- src/components/pages/ManageCourts.js
+++ src/components/pages/ManageCourts.js
@@ -2,16 +2,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Navigation from "../Navigation";
 import Court from "../Court";
+import { Button } from "../Button";
 
-function Book() {
+function ManageCourts() {
+  let i = 0;
   const [dataArray, setDataArray] = useState([]);
+
+  const handleDelete = async (i) => {
+    i--;
+    console.log(dataArray[i].id);
+    await axios({
+      url: "http://localhost:8080/fields",
+      method: "DELETE",
+      data: {
+        id: dataArray[i].id,
+      },
+    }).catch((err) => console.log(err));
+    window.location.reload();
+  };
+
+  let us = localStorage.getItem("username");
   axios({
-    url: "http://localhost:8080/fields",
+    url: `http://localhost:8080/owner/${us}`,
     method: "GET",
     // TODO Token?????????
-  }).then((res) => {
-    setDataArray(res.data);
-  });
+  })
+    .then((res) => {
+      setDataArray(res.data);
+    })
+    .catch((err) => console.log(err));
   return (
     <div>
       <div>
@@ -28,16 +47,28 @@ function Book() {
           className="courts-container"
           style={{
             marginTop: "10%",
-            width: "30%",
+            width: "50%",
             position: "relative",
           }}
         >
-          {dataArray.map((item) => (
-            <Court obiect={item} key={item.key} />
-          ))}
+          {dataArray.map(
+            (item) => (
+              <div>
+                <Court obiect={item} key={item.key} />
+                <div className="butoane">
+                  <button> Modify </button>
+                  <button id={"buton" + i} onClick={() => handleDelete(i)}>
+                    {" "}
+                    Delete{" "}
+                  </button>
+                </div>
+              </div>
+            ),
+            i++
+          )}
         </div>
       </div>
     </div>
   );
 }
-export default Book;
+export default ManageCourts;
